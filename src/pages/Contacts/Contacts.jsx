8827f8b3e97adc7a1d81606f698e49e2dc03fdf1
Raúl Fabra <fabraraul@yahoo.com>
2024-09-23 14:45:17 +0200

    Update Contact routes

---
+++ src/pages/Contacts/Contacts.jsx
@@ -0,0 +1,9 @@
+import { Main } from "../../styles/stylesComponents";
+
+export const Contacts = () => {
+  return (
+    <Main $layout>
+      <h1>HOLA SOY LA TABLA DE CONTACT</h1>
+    </Main>
+  );
+};
