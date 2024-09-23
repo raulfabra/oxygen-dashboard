0c9a377d637e5799ecc47b91616c56d0b30efbdf
Raúl Fabra <fabraraul@yahoo.com>
2024-09-23 10:36:27 +0200

    Improve testing with cypress

--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -1,45 +1,39 @@
-describe("Test login", () => {
-  it("sucessfully loads", () => {
-    cy.visit("http://localhost:5173/");
-  });
-
-  it("check for protected routes", () => {
-    cy.visit("http://localhost:5173/rooms");
-    cy.url().should("include", "/login");
-  });
-
-  it("sucessfully redirects to login", () => {
-    cy.visit("http://localhost:5173/");
-    cy.url().should("include", "/login");
-  });
-
-  it("test for invalid user", () => {
+describe("Login page", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:5173/login");
+  });
 
+  it("frontpage can be opened", () => {
+    cy.contains("HOTEL MIRANDA DASHBOARD").should("be.visible");
+  });
+
+  it("login form can be seen", () => {
+    cy.get("form").should("be.visible");
+  });
+
+  it("login fails with wrong password", () => {
     cy.get('input[type="email"]').type("manolo@gmail.com");
-    cy.get('input[type="password"]').type("1234");
-    cy.get('input[type="password"]').type(`{enter}`);
-
-    cy.get("span").should("contain", "Identificated don't match.");
+    cy.get('input[type="password"]').type("soyunGnomo");
+    cy.get('button[type="submit"]').should("contain", "LOGIN").click();
+    cy.contains("Identificated don't match.").should("be.visible");
   });
 
-  it("test for invalid password", () => {
-    cy.visit("http://localhost:5173/login");
-
-    cy.get('input[type="email"]').type("employer33@gmail.com");
-    cy.get('input[type="password"]').type("admin");
-    cy.get('input[type="password"]').type(`{enter}`);
-
-    cy.get("span").should("contain", "Identificated don't match.");
-  });
-
-  it("test for valid login", () => {
-    cy.visit("http://localhost:5173/login");
-
+  it("user can be login", () => {
     cy.get('input[type="email"]').type("employer33@gmail.com");
     cy.get('input[type="password"]').type("1234");
-    cy.get('input[type="password"]').type(`{enter}`);
+    cy.get('button[type="submit"]').should("contain", "LOGIN").click();
 
     cy.url().should("equal", "http://localhost:5173/dashboard");
+  });
+
+  describe("when not logged in", () => {
+    beforeEach(() => {
+      window.localStorage.clear();
+    });
+    it("user can not been going to dashboard app", () => {
+      cy.visit("http://localhost:5173/rooms");
+      cy.url().should("include", "/login");
+      cy.contains("HOTEL MIRANDA DASHBOARD").should("be.visible");
+    });
   });
 });
