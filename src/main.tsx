fc7a6c4193e3d74a7825d09dec24f07ed6ed3419
Raúl Fabra <fabraraul@yahoo.com>
2024-09-26 19:41:09 +0200

    working in transform the aplication. Change extensions of components

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { AuthProvider } from "./app/Providers/AuthProvider.jsx";
+import { AuthProvider } from "./app/Providers/AuthProvider.js";
 import { store } from "./app/store.js";
-import App from "./App.jsx";
+import App from "./App.js";
 import { GlobalStyle } from "./styles/GlobalStyle.js";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <AuthProvider>
