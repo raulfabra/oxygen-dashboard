c2f581879daba5e4138392193ab8243e7d0a79a7
Raúl Fabra <fabraraul@yahoo.com>
2024-09-26 10:30:12 +0200

    refactor customHook Loading Data in customers

--- src/pages/Customers/Customers.jsx
+++ src/pages/Customers/Customers.jsx
@@ -1,23 +1,13 @@
-import { useEffect, useState } from "react";
-import { CardReview } from "../../components/CardOfReviews/CardReview";
-import { CreateElement, FilterTable, Main, NavTable, OptionsFiltered } from "../../styles/stylesComponents";
-import db_json from "../../json/dataCustomer.json";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { getCustomersListData, getCustomersListStatus, getCustomersListError } from "../../redux/customers/CustomersSlice";
 import { getCustomersThunk } from "../../redux/customers/CustomersThunk";
-import { PaginationProvider } from "../../app/Providers/PaginationProvider";
+import { useLoadingData } from "../../hook/useLoadingData";
+import { CardReview } from "../../components/CardOfReviews/CardReview";
 import { Table } from "../../components/Table/Table";
+import { PaginationProvider } from "../../app/Providers/PaginationProvider";
+import { FilterTable, Main, NavTable, OptionsFiltered } from "../../styles/stylesComponents";
 
 export const Customers = () => {
-  const dispatch = useDispatch();
-  const navigator = useNavigate();
-
-  const [reviewsList, setReviewsList] = useState(null);
-
-  const customersData = useSelector(getCustomersListData);
-  const customersStatus = useSelector(getCustomersListStatus);
-  const customersError = useSelector(getCustomersListError);
+  const { dataJson, refreshData } = useLoadingData(getCustomersListData, getCustomersListError, getCustomersListStatus, getCustomersThunk);
 
   const columns = [
     {
@@ -64,41 +54,19 @@ export const Customers = () => {
     return stars;
   }
 
-  const handleOption = (event) => {
-    const name = event.target.textContent;
-
-    if (name === "All Customer Reviews") {
-      setUsersListData(db_json);
-    }
-    if (name === "Archived") {
-      const newData = db_json.filter((user) => user.statusEmployeer === "Archived");
-
-      setUsersListData(newData);
-    }
-  };
-
-  useEffect(() => {
-    if (customersStatus === "idle") dispatch(getCustomersThunk());
-    else if (customersStatus === "rejected") console.log(customersError);
-    else if (customersStatus === "fulfilled") setReviewsList(customersData);
-  }, [customersStatus]);
-
   return (
     <Main $layout>
       <CardReview />
       <NavTable>
         <FilterTable>
-          <OptionsFiltered onClick={handleOption}>All Employee</OptionsFiltered>
-          <OptionsFiltered onClick={handleOption}>Active Employee</OptionsFiltered>
-          <OptionsFiltered onClick={handleOption}>Inactive Employee</OptionsFiltered>
+          <OptionsFiltered>All Employee</OptionsFiltered>
+          <OptionsFiltered>Active Employee</OptionsFiltered>
+          <OptionsFiltered>Inactive Employee</OptionsFiltered>
         </FilterTable>
-        <CreateElement type="button" onClick={() => navigator("/createUser")}>
-          + New Employee
-        </CreateElement>
       </NavTable>
-      {reviewsList && (
+      {dataJson && (
         <PaginationProvider>
-          <Table data={reviewsList} columns={columns} />
+          <Table data={dataJson} columns={columns} />
         </PaginationProvider>
       )}
     </Main>
