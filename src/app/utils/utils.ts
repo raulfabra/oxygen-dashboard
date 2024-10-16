9bd1a3af1742b2d9394219108f54ed03cd6b9e7e
raulfabra <fabraraul@yahoo.com>
2024-10-16 23:21:50 +0200

    type date

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -5,3 +5,14 @@ export const mockUser = {
   email: "employer33@gmail.com",
   password: "1234",
 };
+
+export const getFormatDate = (date: Date) => {
+  const fecha = new Date(date);
+
+  const day = String(fecha.getDate()).padStart(2, "0");
+  const month = String(fecha.getMonth() + 1).padStart(2, "0");
+  const year = fecha.getFullYear();
+
+  const dateFormat = `${day}/${month}/${year}`;
+  return dateFormat;
+};
